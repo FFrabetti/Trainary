a64d865112b08f5bdf16ffa53cb3a608c9f38aaa
FFrabetti <frabettifil@gmail.com>
2017-05-01 13:08:51 +0200

    Aggiunta di Attivita e Categoria

---
+++ Attivita.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Trainary
+{
+    class Attivita
+    {
+        private string _nome;
+        private string _descrizione;
+        private string[] _attrezzi;
+        private Categoria _categoria;
+
+        public Attivita(string nome, Categoria categoria, string descrizione, string[] attrezzi)
+        {
+            if (nome == null)
+                throw new ArgumentNullException("nome");
+            if (categoria == null)
+                throw new ArgumentNullException("categoria");
+            if (attrezzi == null)
+                throw new ArgumentNullException("attrezzi");
+
+            _nome = nome;
+            _categoria = categoria;
+            _descrizione = descrizione;
+            _attrezzi = attrezzi;
+        }
+
+        public Attivita(string nome, Categoria categoria, string descrizione)
+            : this(nome, categoria, descrizione, new string[0]) { }
+
+        public Attivita(string nome, Categoria categoria, string[] attrezzi)
+            : this(nome, categoria, null, attrezzi) { }
+
+        public Attivita(string nome, Categoria categoria)
+            : this(nome, categoria, (string)null) { }
+
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+        }
+
+        public string Descrizione
+        {
+            get
+            {
+                return _descrizione;
+            }
+        }
+
+        public string[] Attrezzi
+        {
+            get
+            {
+                return _attrezzi;
+            }
+        }
+
+        public Categoria Categoria
+        {
+            get
+            {
+                return _categoria;
+            }
+        }
+
+        public override string ToString()
+        {
+            return _nome;
+        }
+    }
+}
---
+++ Categoria.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainary
+{
+    class Categoria
+    {
+        private string _nome;
+        private ISet<Attivita> _attivita;
+        private ISet<Categoria> _sottoCategorie;
+
+        public Categoria(string nome, ISet<Attivita> attivita, ISet<Categoria> sottoCategorie)
+        {
+            if (nome == null)
+                throw new ArgumentNullException("nome");
+            if (attivita == null)
+                throw new ArgumentNullException("attivita");
+
+            _nome = nome;
+            _attivita = attivita;
+            _sottoCategorie = sottoCategorie;
+        }
+
+        public Categoria(string nome, ISet<Attivita> attivita) 
+            : this(nome, attivita, null) { }
+
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+        }
+
+        public ISet<Attivita> Attivita
+        {
+            get
+            {
+                return _attivita;
+            }
+        }
+
+        public ISet<Categoria> SottoCategorie
+        {
+            get
+            {
+                return _sottoCategorie;
+            }
+        }
+
+        public override string ToString()
+        {
+            return _nome;
+        }
+    }
+}
