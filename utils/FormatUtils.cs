9ec71defbba46144249bad728315eb4a0116ff95
FFrabetti <frabettifil@gmail.com>
2017-05-14 18:25:00 +0200

    Aggiunte classi di utilità
    Per gestire gli enumerativi (tra cui Units per le unità di misura) e il
    formato dei numeri come stringhe

---
+++ utils/FormatUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainary
+{
+    static class FormatUtils
+    {
+        public static string ToMaxTwoDecimals(double value)
+        {
+            return value.ToString("0.##");
+        }
+    }
+}
