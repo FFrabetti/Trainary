8e6a05c71fbae1d28fcb63cdeb85c89068d41e89
andreab95 <andreabonantini@gmail.com>
2017-05-16 12:03:56 +0200

    Aggiunta classi: Diario, FiltroAllenamenti, FiltroPerScheda
    Non ho fatto il FiltroPerScheda con Linq perché non saprei come fare...
    Inoltre probabilmente ci saranno cose da modificare, però intanto carico
    così

---
+++ Diario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainary
+{
+    public class Diario
+    {
+        private readonly List<Allenamento> _allenamenti;
+
+        private static Diario _instance;
+
+        private Diario()
+        { }
+
+        public static Diario GetInstance()
+        {
+            if (_instance == null)
+                _instance = new Diario();
+            return _instance;
+        }
+
+        public List<Allenamento> Allenamenti
+        {
+            get
+            {
+                return _allenamenti;
+            }
+        }
+
+        public Allenamento[] FiltraAllenamenti(FiltroAllenamenti filtro)
+        {
+            if (filtro == null)
+                throw new ArgumentNullException("filtro");
+            return filtro.Filtra(_allenamenti);
+        }
+    }
+}
