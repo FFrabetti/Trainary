3d5938d1d2ece5e33707a6509fc9a459dda7358a
FFrabetti <frabettifil@gmail.com>
2017-06-02 23:12:43 +0200

    ListBoxControl

---
+++ presenter/ListBoxPresenter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Trainary.view;
+
+namespace Trainary.presenter
+{
+    public class ListBoxPresenter<T>
+    {
+        private ListBoxControl _control;
+        private List<T> _items;
+        private BindingList<T> bindingList;
+
+        public ListBoxPresenter(ListBoxControl listControl, IEnumerable<T> items)
+        {
+            if (listControl == null)
+                throw new ArgumentNullException("listControl");
+            if (items == null)
+                throw new ArgumentNullException("items");
+
+            _control = listControl;
+            _items = new List<T>(items);
+
+            bindingList = new BindingList<T>(_items);
+            _control.ListBox.DataSource = bindingList;
+
+            _control.ListBox.SelectedValueChanged += SelectionHandler;
+            _control.RemoveButton.Click += RemoveButtonHandler;
+            SelectionHandler(null, null);
+        }
+
+        private void SelectionHandler(object sender, EventArgs e)
+        {
+            _control.RemoveButton.Enabled = SelectedItems.Count > 0;
+        }
+
+        private void RemoveButtonHandler(object sender, EventArgs e)
+        {
+            if (SelectedItems.Count > 0)
+                RemoveItem(SelectedItem);
+        }
+
+        public ListBoxControl ListBoxControl
+        {
+            get { return _control; }
+        }
+
+        public T SelectedItem
+        {
+            get
+            {
+                return (T)(_control.ListBox.SelectedItem ?? default(T));
+            }
+        }
+
+        public IList SelectedItems
+        {
+            get
+            {
+                return _control.ListBox.SelectedItems;
+            }
+        }
+
+        public void AddItem(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException("item");
+
+            _items.Add(item);
+            bindingList.ResetBindings();
+        }
+
+        public void RemoveItem(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException("item");
+
+            string messageBoxText = "Sicuro di voler cancellare '" + item + "'?";
+            string caption = "Conferma cancellazione";
+            MessageBoxButtons buttons = MessageBoxButtons.OKCancel;
+            MessageBoxIcon icon = MessageBoxIcon.Warning;
+
+            if (MessageBox.Show(messageBoxText, caption, buttons, icon) == DialogResult.OK)
+            {
+                _items.Remove(item);
+                bindingList.ResetBindings();
+            }
+        }
+
+    }
+}
---
+++ view/ListBoxControl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trainary.view
+{
+    public partial class ListBoxControl : UserControl
+    {
+        public ListBoxControl()
+        {
+            InitializeComponent();
+        }
+
+        public Label TitleLabel
+        {
+            get { return _label; }
+        }
+
+        public Panel ButtonsPanel
+        {
+            get { return _buttonsPanel; }
+        }
+
+        public Button AddButton
+        {
+            get { return _addButton; }
+        }
+
+        public Button RemoveButton
+        {
+            get { return _removeButton; }
+        }
+
+        public ListBox ListBox
+        {
+            get { return _listBox; }
+        }
+    }
+}
