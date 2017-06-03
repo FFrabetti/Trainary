9c3e487fad477d7a9b3774a54f1ebc8bd26c58c4
Azzurra95 <azzurra.ciliberti@gmail.com>
2017-06-03 16:09:22 +0200

    Modifiche filtri

---
+++ model/FiltroPerScheda.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Trainary.model;
+using Trainary.utils;
+
+namespace Trainary
+{
+
+    public static partial class FiltroFactory
+    {
+        [Label("Filtro per scheda")]
+        private class FiltroPerScheda : IFiltroAllenamenti
+        {
+            
+            public FiltroPerScheda()
+            { }
+
+            public IEnumerable<Allenamento> Filtra(IEnumerable<Allenamento> listaAllenamenti, object opzione)
+            {
+                if (!(opzione is Scheda))
+                    throw new ArgumentException("opzione is not Scheda");
+                Scheda scheda = (Scheda)opzione;
+
+                return (from AllenamentoProgrammato allenamento in listaAllenamenti.OfType<AllenamentoProgrammato>()
+                        where allenamento.Seduta.Scheda.Equals(scheda)
+                        select allenamento);
+            }
+        }
+    }
+}
---
+++ model/FiltroPerSeduta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Trainary.model;
+using Trainary.utils;
+
+namespace Trainary
+{
+    
+    public static partial class FiltroFactory
+    {
+        [Label("Filtro per seduta")]
+        private class FiltroPerSeduta : IFiltroAllenamenti
+        {
+            
+            public FiltroPerSeduta()
+            { }
+
+            public IEnumerable<Allenamento> Filtra(IEnumerable<Allenamento> listaAllenamenti, object opzione)
+            {
+                if (!(opzione is Seduta))
+                    throw new ArgumentException("opzione is not Seduta");
+                Seduta seduta = (Seduta) opzione;
+
+                return
+                    from AllenamentoProgrammato allenamento in listaAllenamenti.OfType<AllenamentoProgrammato>()
+                    where seduta.Equals(allenamento.Seduta)
+                    select allenamento;
+            }
+        }
+    }
+}
