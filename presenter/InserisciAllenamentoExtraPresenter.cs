e12159f3328c2da3df8e2ac9bf0899f06ad6cf77
FFrabetti <frabettifil@gmail.com>
2017-06-24 22:49:38 +0200

    Ora compila, anche se ci sto ancora lavorando

--- presenter/InserisciAllenamentoExtraPresenter.cs
+++ presenter/InserisciAllenamentoExtraPresenter.cs
@@ -28,13 +28,13 @@ namespace Trainary.presenter
 
         private void OnAggiungiCircuitoClick(object sender, EventArgs e)
         {
-            IList<Esercizio> esercizi = new List<Esercizio>();
+            IList<Esercizio> esDisponibili = new List<Esercizio>();
             foreach(EsercizioSvolto es in EserciziSvolti)
-                esercizi.Add(es.Esercizio);
+                esDisponibili.Add(es.Esercizio);
 
             using (NewCircuitoForm form = new NewCircuitoForm())
             {
-                NewCircuitoPresenter presenter = new NewCircuitoPresenter(form, esercizi);
+                NewCircuitoPresenter presenter = new NewCircuitoPresenter(form, esDisponibili);
                 if (form.ShowDialog() == DialogResult.OK)
                 {
                     Circuito circuito = presenter.NewCircuito();
@@ -57,80 +57,41 @@ namespace Trainary.presenter
                     AggiornaTreeView();
                 }
             }
-            
         }
 
         private void OnAggiungiEsercizioClick(object sender, EventArgs e)
         {
-           
-            SvolgiVisitor sv = new SvolgiVisitor();
-          
-            Esercizio esercizio;
             using (NewEsercizioForm newEsForm = new NewEsercizioForm())
             {
                 NewEsercizioPresenter presenter = new NewEsercizioPresenter(newEsForm);
 
                 if (newEsForm.ShowDialog() == DialogResult.OK)
                 {
-                    esercizio = presenter.NewEsercizio();
-                    foreach (EsercizioSvolto svolto in EserciziSvolti)
-                    {
-                        if (svolto.Esercizio.Equals(esercizio))
-                        {
-                            string messageBoxText = "Non è possibile aggiungere un esercizio più volte nello stesso allenamento";
-                            string caption = "Errore";
-                            MessageBoxButtons buttons = MessageBoxButtons.OK;
-                            MessageBoxIcon icon = MessageBoxIcon.Warning;
+                    Esercizio esercizio = presenter.NewEsercizio();
 
-                            MessageBox.Show(messageBoxText, caption, buttons, icon);
-                            return;
-                        }
-                    }
+                    SvolgiVisitor sv = new SvolgiVisitor();
                     esercizio.Accept(sv);
                     EsercizioSvolto ev = sv.EsercizioSvolto;
                     EserciziSvolti.Add(ev);
                     AggiornaTreeView();
                 }
-               
             }
-          
-            
         }
-
-       
 
         private void OkButtonClick(object sender, EventArgs e)
         {
-            DateTime data = Form.Data.Value;
-            string nome = _control.Nome.Text;
-            AllenamentoExtra allenamentoExtra;
             try
             {
-                allenamentoExtra = new AllenamentoExtra(data, EserciziSvolti.ToArray(), nome);
+                DateTime data = Form.Data.Value;
+                string nome = _control.Nome.Text;
+                Allenamento allenamentoExtra = new AllenamentoExtra(data, EserciziSvolti.ToArray(), nome);
+                Diario.GetInstance().Allenamenti.Add(allenamentoExtra);
+                Form.Close();
             }
             catch (Exception ex)
             {
-                string messageBoxText = ex.Message;
-                string caption = "Errore";
-                MessageBoxButtons buttons = MessageBoxButtons.OK;
-                MessageBoxIcon icon = MessageBoxIcon.Warning;
-
-                MessageBox.Show(messageBoxText, caption, buttons, icon);
-                return;
+                MessageBoxUtils.DisplayError(ex.Message);
             }
-            Diario.GetInstance().Allenamenti.Add(allenamentoExtra);
-            Form.Close();
-        }
-        protected override void OnAnnullaSelezioneButtonClick(object sender, EventArgs e)
-        {
-            base.OnAnnullaSelezioneButtonClick(sender, e);
-            _control.Nome.Text = String.Empty;
-        }
-        protected override void OnApplicationIdle(object sender, EventArgs e)
-        {
-            base.OnApplicationIdle(sender, e);
-            Form.AnnullaSelezioneButton.Enabled = Form.Data.Value.Date != DateTime.Today.Date || EserciziSvolti.Count > 0 || _control.Nome.Text != String.Empty;
-
         }
     }
 }
